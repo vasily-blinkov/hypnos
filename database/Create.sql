95f1fe21ae99c597772989856565aa36328d3b1e
Vasily Blinkov <vasily.blinkov@gmail.com>
2023-02-17 22:33:06 +0300

    Database creation

--- database/Create.sql
+++ database/Create.sql
@@ -3,10 +3,14 @@
 -- You can set values of the variables below to customize the database.
 DECLARE @database_name NVARCHAR(10) = N'Hypnos';
 
+-- Variables for internal usage.
+DECLARE @sql_string NVARCHAR(500);
+
 USE [master];
 
 IF DB_ID(@database_name) IS NULL
 BEGIN
-	PRINT 'Creating database ''Hypnos''.';
+	PRINT N'Creating database ''' + @database_name + '''';
+	SET @sql_string = N'CREATE DATABASE ' + @database_name;
+	EXECUTE sp_executesql @sql_string;
 END
-
